4f809e6158617a46667dcc6f7527ebe9d3211409
Craig Roberts <craig.roberts11@outlook.com>
2022-12-15 23:13:12 +1030

    Working password generator

--- script.js
+++ script.js
@@ -12,68 +12,62 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-// create array of strings for each lowercase letter of alphabet
-const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const characterTypes = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  uppercase: 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split(''),
+  numerical: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  special: [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+}
 
-// create uppcase array using similar method
-const uppercase = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+function generatePassword() {
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const special = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-',
-'.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
-
-const generatePassword = () => {
-// object to track the user choices for password generation
-const props = {
-  passwordLength: 0,
-  uppercase: false,
-  lowercase: false,
-  numeric: false,
-  specialChars: false,
-};
-
-while (props.passwordLength < 8 || props.passwordLength > 128) {
+let passwordLength = 0;
+const password = [];
+let selectedTypes = [];
+ 
+// Prompt user to choose password length
+while (passwordLength < 8 || passwordLength > 128) {
   let numChars = prompt('Choose a password length between 8 and 128 characters:');
 
   // only modify props.passwordLength if within the required range, otherwise repeat until user does so 
   if (numChars >= 8 && numChars <= 128) {
-     props.passwordLength = numChars;
+     passwordLength = numChars;
   }
 }
 
-
-let numSelections = 0;
-do{
-
-props.lowercase = confirm("Use lower case letters?")
-if (props.lowercase) {
-  numSelections++;
+// Continue prompting user until they have chosen at least one character type
+while (selectedTypes.length === 0) {
+  selectedTypes = promptUser();
 }
 
-props.uppercase = confirm("Use upper case letters?")
-if (props.uppercase) {
-  numSelections++;
+// Generate random password 
+for (let i = 0; i < passwordLength; i++) {
+  const type = randomInteger(selectedTypes.length);
+  const index = randomInteger(selectedTypes[type].length);
+
+  password.push(selectedTypes[type][index]);
 }
 
-props.numeric = confirm("Use numbers?")
-if (props.numeric) {
-  numSelections++;
+// Convert password array into string
+
+const passwordString = password.join('');
+
+  return passwordString;
 }
 
-props.specialChars = confirm("Use special characters?")
-if (props.specialChars) {
-  numSelections++;
-}
-}
-while (numSelections === 0);
+// iterates over characterTypes and prompts user to select each one individually
+function promptUser() {
+  const selected = [];
 
-// push generated password into array
-// join together into string
+  for(const type in characterTypes) {
+    if(confirm(`Use ${type} characters?`)) {
+      selected.push(characterTypes[type]);
+    }
+  }
 
-
-console.log(numSelections);
-
-  
+  return selected;
 }
 
+function randomInteger(max) {
+  return Math.floor(Math.random() * max);
+}
